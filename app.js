0d1ed283bc697960c589bc3bf779b458044163d1
Sourav Choudhary <84680538+sourav0098@users.noreply.github.com>
2022-11-24 11:04:55 -0500

    added delete restaurant

--- app.js
+++ app.js
@@ -19,36 +19,76 @@ mongoose
     console.log(err);
   });
 
+// Initializing Model
 const Restaurant = require("./models/Restaurant");
 
-async function getAllRestaurants(page,perPage,borough) {
+async function getAllRestaurants(page, perPage, borough) {
   try {
-    if(borough==undefined){
-      const restaurants = await Restaurant.find().sort({restaurant_id:1}).limit(perPage).skip((page-1)*perPage)
-      return await restaurants;
-    }else{
-      const restaurants = await Restaurant.find({borough:borough}).sort({restaurant_id:1}).limit(perPage).skip((page-1)*perPage)
-      return await restaurants;
+    if (borough == undefined) {
+      const restaurants = await Restaurant.find()
+        .sort({ restaurant_id: 1 })
+        .limit(perPage)
+        .skip((page - 1) * perPage);
+
+      // If no matching records found
+      if (restaurants.length == 0) {
+        return { message: "No records Found" };
+      } else {
+        return restaurants;
+      }
+    } else {
+      const restaurants = await Restaurant.find({ borough: borough })
+        .sort({ restaurant_id: 1 })
+        .limit(perPage)
+        .skip((page - 1) * perPage);
+
+      // If no matching records found
+      if (restaurants.length == 0) {
+        return { message: "No records Found" };
+      } else {
+        console.log(restaurants);
+        return restaurants;
+      }
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.log(err);
   }
 }
 
-app.get("/api/restaurants", function (req, res) {
-  let page=req.query.page;
-  let perPage=req.query.perPage;
-  let borough=req.query.borough;
-  getAllRestaurants(page,perPage,borough).then((data)=>{
-    res.json(data)
-  }).catch((err)=>{
+async function deleteRestaurantById(id) {
+  try{
+    const restaurant=await Restaurant.deleteOne({id});
+    return `${restaurant.name} restaurant has been deleted successfully`
+  }catch(err){
     console.log(err)
-  })
+  }
+}
+
+app.get("/api/restaurants", function (req, res) {
+  let page = req.query.page;
+  let perPage = req.query.perPage;
+  let borough = req.query.borough;
+
+  getAllRestaurants(page, perPage, borough)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Delete a record
+app.delete("/api/restaurants/id=:id", (req, res) => {
+  let id = req.params.id;
+  deleteRestaurantById(id)
+    .then((data) => {
+      res.send("Record has been deleted successfully");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(port);
-console.log("App listening on port : " + port);
-
-app.listen(port);
-console.log("App listening on port : " + port);
+console.log("Listening on port : " + port);
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 require('dotenv').config()
 module.exports = {
-    url : `mongodb+srv://sourav1998:${process.env.PASSWORD}@cluster0.nwlkm13.mongodb.net/sample_restaurants?retryWrites=true&w=majority1`
+    url : `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.nwlkm13.mongodb.net/?retryWrites=true&w=majority`
 };
